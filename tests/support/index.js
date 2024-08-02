0cf8c50846e0777d5e18279e01b3c1f43304c844
--get <leonardo.ignacio@inmetrics.com.br>
2024-08-01 22:31:59 -0300

    adequacoes a nova build

--- tests/support/index.js
+++ tests/support/index.js
@@ -1,9 +1,15 @@
+// Esta classe é responsável por fazer com que a instância 'page'
+// do playwright tenha total acesso aos page objects/actions
+// Também faz com que a instância 'request' do playwright já receba 
+// o token de login no início de qualquer execução apenas uma vez,
+// não necessitando informar o token em todas as chamadas de API
+
 const { test: base, expect } = require('@playwright/test')
 
 const { Leads } = require('./actions/Leads')
 const { Login } = require('./actions/Login')
 const { Movies } = require('./actions/Movies')
-const { Toast } = require('./actions/Components')
+const { Popup } = require('./actions/Components')
 
 const { Api } = require('./api')
 
@@ -14,7 +20,7 @@ const test = base.extend({
         context['leads'] = new Leads(page)
         context['login'] = new Login(page)
         context['movies'] = new Movies(page)
-        context['toast'] = new Toast(page)
+        context['popup'] = new Popup(page)
 
         await use(context)
     },
