ac1fe8254bc1c81e94cdae343af0e96d335447ef
--get <leonardo.ignacio@inmetrics.com.br>
2024-08-02 16:57:00 -0300

    novos cenarios de teste

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,13 +1,13 @@
 const { test, expect } = require('../support')
 
 const data = require('../support/fixtures/movies.json')
-const {executeSQL} = require('../support/database')
+const { executeSQL } = require('../support/database')
 
 test.beforeAll(async () => {
     await executeSQL(`DELETE from movies`)
 })
 
-test('deve poder cadastrar um novo filme', async ({ page }) => {    
+test('deve poder cadastrar um novo filme', async ({ page }) => {
     const movie = data.create
 
     await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
@@ -15,7 +15,16 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
     await page.popup.haveText(`O filme '${movie.title}' foi adicionado ao catálogo.`)
 })
 
-test('não deve cadastrar quando o título é duplicado', { tag: ['@tst']}, async ({ page, request }) => {    
+test('deve poder remover um filme', async ({ page, request }) => {
+    const movie = data.to_remove
+    await request.api.postMovie(movie)
+
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.remove(movie.title)
+    await page.popup.haveText('Filme removido com sucesso.')
+})
+
+test('não deve cadastrar quando o título é duplicado', { tag: ['@tst'] }, async ({ page, request }) => {
     const movie = data.duplicate
 
     await request.api.postMovie(movie)
@@ -36,4 +45,16 @@ test('não deve cadastrar quando os campos obrigatórios não são preenchidos',
         'Campo obrigatório',
         'Campo obrigatório'
     ])
+})
+
+test('deve realizar busca pelo termo zumbi', async ({ page, request }) => {
+    const movies = data.search
+
+    movies.data.forEach(async (m) => {
+        await request.api.postMovie(m)
+    })
+
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.search(movies.input)
+    await page.movies.tableHave(movies.outputs)
 })
